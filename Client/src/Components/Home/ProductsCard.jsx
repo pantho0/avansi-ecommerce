45589be626b9b68e96cc58125d84f2b88e8740be
pantho0 <panthomashreky6@gmail.com>
2024-02-09 17:59:46 +0600

    products image added

--- Client/src/Components/Home/ProductsCard.jsx
+++ Client/src/Components/Home/ProductsCard.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 
 const ProductsCard = () => {
-    const [products, setProducts] = useState();
+    const [products, setProducts] = useState([]);
     useEffect(()=>{
         fetch('products.json')
         .then(res=>res.json())
@@ -16,22 +16,22 @@ const ProductsCard = () => {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
             {
-                products.map(product => <div key={product.name} className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                products.map(product => <div key={product.name} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm">
                 <a className="block relative h-48 rounded overflow-hidden">
                   <img
                     alt="ecommerce"
-                    className="object-cover object-center w-full h-full block"
-                    src="https://dummyimage.com/420x260"
+                    className="object-contain object-center w-full h-full block"
+                    src={product.image}
                   />
                 </a>
                 <div className="mt-4">
                   <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    CATEGORY
+                    {product.parent_category}
                   </h3>
                   <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The Catalyzer
+                    {product.name}
                   </h2>
-                  <p className="mt-1">$16.00</p>
+                  <p className="mt-1 text-primary font-bold">${product.price}</p>
                 </div>
               </div>)
             }

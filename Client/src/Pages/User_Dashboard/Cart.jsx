05d733da077c711fafc6975add26e5ac9e1fca13
pantho0 <panthomashreky6@gmail.com>
2024-02-17 14:01:20 +0600

    cart item page & billing summary components added

--- Client/src/Pages/User_Dashboard/Cart.jsx
+++ Client/src/Pages/User_Dashboard/Cart.jsx
@@ -42,33 +42,62 @@ const Cart = () => {
       {/* Cart Summary */}
       <div className="flex z-10">
         <div className="w-[66%] h-screen bg-gary-300 m-5 shadow-2xl rounded-2xl">
-          {products.map((item, idx) => (
-            <div className="flex items-center border-b-2 gap-2 p-8" key={item?._id}>
-              <div>
-                
-                <div className="avatar gap-2">
-                  <div>
-                  <p>{idx+1}.</p>
-                </div>
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src={item?.image} />
+          {
+            products.length === 0  ? <div><p className="flex w-full items-center justify-center h-[calc(100vh-30vh)] font-bold">No Products Added Yet</p></div> : 
+            <>
+            {products.map((item, idx) => (
+              <div className="flex items-center border-b-2 gap-2 p-8" key={item?._id}>
+                <div>
+                  
+                  <div className="avatar gap-2">
+                    <div>
+                    <p>{idx+1}.</p>
+                  </div>
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img src={item?.image} />
+                    </div>
                   </div>
                 </div>
+                <div className="flex-grow">
+                  <p className="font-bold">{item?.name}</p>
+                  <p>{item?.price}</p>
+                </div>
+                <div className="">
+                  <button onClick={()=>handleDelete(item._id)} className="btn btn-primary btn-xs">
+                    <MdDeleteSweep size={18} color="white"/>
+                  </button>
+                </div>
               </div>
-              <div className="flex-grow">
-                <p className="font-bold">{item?.name}</p>
-                <p>{item?.price}</p>
-              </div>
-              <div className="">
-                <button onClick={()=>handleDelete(item._id)} className="btn btn-primary btn-xs">
-                  <MdDeleteSweep size={18} color="white"/>
-                </button>
-              </div>
-            </div>
-        
-          ))}
+          
+            ))}
+            </>
+          }
         </div>
-        <div className="bg-blue-300 flex-1 h-screen m-5 rounded-2xl shadow-2xl"></div>
+        <div className="flex-1 m-5 h-1/2 rounded-2xl shadow-2xl">
+          <div className="text-center p-4 bg-primary text-white rounded-tr-2xl rounded-tl-2xl">
+            <p>Billing Summary</p>
+          </div>
+          <div className="flex justify-between px-4 pt-4 text-sm text-gray-500">
+            <p>Total Price</p>
+            <p>$0.0</p>
+          </div>
+          <div className="flex justify-between px-4 pt-4 text-sm text-gray-500">
+            <p>Total Price Discount</p>
+            <p>$0.0</p>
+          </div>
+          <div className="flex justify-between px-4 pt-4 text-sm text-gray-500">
+            <p>Vat & Tax</p>
+            <p>$0.0</p>
+          </div>
+          <hr  className="mt-6"/>
+          <div className="flex justify-between px-4 pt-4 text-sm text-black font-medium">
+            <p>Total Price</p>
+            <p>$0.0</p>
+          </div>
+         <div className="p-3 mt-4">
+         <button className="btn btn-primary btn-md rounded-md w-full">Check Out</button>
+         </div>
+        </div>
       </div>
     </div>
   );

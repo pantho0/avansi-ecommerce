393873ea933280dd90471caf768b92b92564b101
pantho0 <panthomashreky6@gmail.com>
2024-02-21 20:49:31 +0600

    dashboard drawer button poisition updated tablet responsive improved

--- Client/src/Layouts/Dashboard.jsx
+++ Client/src/Layouts/Dashboard.jsx
@@ -18,12 +18,12 @@ const Dashboard = () => {
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
-        <div>
+        <div className="flex justify-end">
           <label
             htmlFor="my-drawer-2"
-            className="btn bg-primary drawer-button fixed lg:hidden"
+            className="btn bg-blue-950 border-gray-400 rounded-none z-10 drawer-button fixed lg:hidden"
           >
-            <MdMenuOpen color="white" size={25} />
+            <MdMenuOpen color="white" size={20} />
           </label>
         </div>
         {/* Page content here */}
